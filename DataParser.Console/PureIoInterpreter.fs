bb7c9a8d193691ba47a860a7eccaa4d56e4464cc
Rex Ng <rexcfnghk@gmail.com>
2026-01-18 22:03:46 +0800

    Fix errors

--- DataParser.Console/PureIoInterpreter.fs
+++ DataParser.Console/PureIoInterpreter.fs
@@ -1,13 +1,13 @@
 namespace DataParser.Console
 
-open System
 open ResultMap
 open DataParser.Console.DataFiles
-open DataParser.Console.FileRead
 
-module PureIoInterpreter =
+module PureIOInterpreter =
+    open Core
+    open FormatFiles
 
-    type Action =
+    type RecordedAction =
         | ReadSpecs of folder: string
         | ParseDataFile of filePath: string
         | WriteOutput of folder: string * DataFileParseResult
@@ -17,25 +17,27 @@ module PureIoInterpreter =
     /// Interpret a FreeIO program purely by invoking provided handlers and recording actions.
     let rec interpretPure (onReadSpecs: string -> ResultMap<FormatName, FormatLine list, Error>)
                           (onParse: DataFileFormat -> Result<DataFileParseResult, Error list>)
-                          (program: FreeIO<'a>) : ('a * Action list) =
+                          (program: FreeIO<'a>) : ('a * RecordedAction list) =
         match program with
         | Pure x -> (x, [])
-        | Free (ReadSpecs(folder, next)) ->
-            let specs = onReadSpecs folder
-            let (res, actions) = interpretPure onReadSpecs onParse (next specs)
-            (res, ReadSpecs folder :: actions)
-        | Free (ParseDataFile(df, next)) ->
-            // choose to call the parse handler and record which file was parsed
-            let parseRes = onParse df
-            let (res, actions) = interpretPure onReadSpecs onParse (next parseRes)
-            let filePathStr = match df.FilePath with FilePath s -> s
-            (res, ParseDataFile filePathStr :: actions)
-        | Free (WriteOutput(folder, result, next)) ->
-            let (res, actions) = interpretPure onReadSpecs onParse (next())
-            (res, WriteOutput(folder, result) :: actions)
-        | Free (LogError(msg, next)) ->
-            let (res, actions) = interpretPure onReadSpecs onParse (next())
-            (res, LogError msg :: actions)
-        | Free (LogInfo(msg, next)) ->
-            let (res, actions) = interpretPure onReadSpecs onParse (next())
-            (res, LogInfo msg :: actions)
+        | Free op ->
+            match op with
+            | IO.ReadSpecs(folder, next) ->
+                let specs = onReadSpecs folder
+                let (res, actions) = interpretPure onReadSpecs onParse (next specs)
+                (res, RecordedAction.ReadSpecs folder :: actions)
+            | IO.ParseDataFile(df, next) ->
+                // choose to call the parse handler and record which file was parsed
+                let parseRes = onParse df
+                let (res, actions) = interpretPure onReadSpecs onParse (next parseRes)
+                let filePathStr = match df.FilePath with FilePath s -> s
+                (res, RecordedAction.ParseDataFile filePathStr :: actions)
+            | IO.WriteOutput(folder, result, next) ->
+                let (res, actions) = interpretPure onReadSpecs onParse (next())
+                (res, RecordedAction.WriteOutput(folder, result) :: actions)
+            | IO.LogError(msg, next) ->
+                let (res, actions) = interpretPure onReadSpecs onParse (next())
+                (res, RecordedAction.LogError msg :: actions)
+            | IO.LogInfo(msg, next) ->
+                let (res, actions) = interpretPure onReadSpecs onParse (next())
+                (res, RecordedAction.LogInfo msg :: actions)
